97837a9d2fa29cbcfc34de4bdb2d3295266b06f9
My Name <tadzhibaevaelnura@gmail.com>
2024-01-02 00:00:56 +0600

    Initial commit

---
+++ src/TwoSum.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        // No solution found
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = twoSum(nums1, target1);
+        System.out.println(Arrays.toString(result1)); // Output: [0, 1]
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = twoSum(nums2, target2);
+        System.out.println(Arrays.toString(result2)); // Output: [1, 2]
+    }
+}
